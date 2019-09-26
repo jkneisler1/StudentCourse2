ecd542021050ec306844a106e1714ff424d8ba44
jkneisler1 <John.Kneisler@gmail.com>
2019-09-26 14:53:04 -0400

    First Submit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home2() {
+        return "home";
+    }
+
+    @RequestMapping("/anotherPage")
+    public String home3() {
+        return "anotherPage";
+    }
+
+}
